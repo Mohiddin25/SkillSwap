78b94496ad5c641f324f56db451c02d58d903a83
KHASIM <khasim2586@gmail.com>
2026-09-10 15:52:46 +0530

    changes in endpoints

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:3000',
   'http://127.0.0.1:5173',
-  'https://swap-skills-frontend.vercel.app'
+  'https://skillswap-gyjl.onrender.com/api'
 ];
 
 if (env.CLIENT_URL) {
