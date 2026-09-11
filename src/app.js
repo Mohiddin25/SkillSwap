4f55cc280e13cc53ef5a5a61a0c5c9e2fb06d907
KHASIM <khasim2586@gmail.com>
2026-09-11 15:33:31 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:3000',
   'http://127.0.0.1:5173',
-  'https://skillswap-gyjl.onrender.com/api'
+  'https://swap-skills-frontend.vercel.app'
 ];
 
 if (env.CLIENT_URL) {
